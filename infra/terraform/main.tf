e9594da6b40bb9b3491323b01c3e91c688b05578
Estibalitz <edr1006@alu.ubu.es>
2026-03-04 15:55:26 +0100

    Initialize terraform infrastructure base

---
+++ infra/terraform/main.tf
@@ -0,0 +1,7 @@
+# Base Terraform configuration
+# Resources will be added incrementally as infrastructure is implemented
+
+locals {
+  project = "plentzia-piraguismo-platform"
+  environment = "dev"
+}
