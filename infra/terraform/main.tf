b1338a073e0a556faa9c4c1898b7441d757c8224
Estibalitz <edr1006@alu.ubu.es>
2026-03-04 16:32:45 +0100

    Add terraform configuration

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -5,3 +5,12 @@ locals {
   project = "plentzia-piraguismo-platform"
   environment = "dev"
 }
+
+resource "aws_s3_bucket" "public_web" {
+  bucket = "${local.project}-public-web"
+
+  tags = {
+    Project = local.project
+    Environment = local.environment
+  }
+}
