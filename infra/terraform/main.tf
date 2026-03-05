223881804a23e271380e66a65a1165585b648d13
Estibalitz <edr1006@alu.ubu.es>
2026-03-05 14:54:04 +0100

    Add DynamoDB tables for pages and news

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -14,3 +14,44 @@ resource "aws_s3_bucket" "public_web" {
     Environment = local.environment
   }
 }
+
+resource "aws_s3_bucket" "media" {
+  bucket = "${local.project}-media"
+
+  tags = {
+    Project     = local.project
+    Environment = local.environment
+  }
+}
+
+resource "aws_dynamodb_table" "pages" {
+  name         = "${local.project}-pages"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "page_id"
+
+  attribute {
+    name = "page_id"
+    type = "S"
+  }
+
+  tags = {
+    Project     = local.project
+    Environment = local.environment
+  }
+}
+
+resource "aws_dynamodb_table" "news" {
+  name         = "${local.project}-news"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "news_id"
+
+  attribute {
+    name = "news_id"
+    type = "S"
+  }
+
+  tags = {
+    Project     = local.project
+    Environment = local.environment
+  }
+}
